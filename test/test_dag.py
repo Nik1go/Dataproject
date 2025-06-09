18d0ab7e0540bffcb7cacf3800887ddb952da9b6
Nik1go <leoja@N15I711-16GR512.localdomain>
2025-06-09 19:31:48 +0200

    clean1

--- test/test_dag.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-dfq = pd.read_parquet("/home/leoja/airflow/data/quadrants.parquet")
-print(dfq.shape)       # Nombre de lignes et colonnes
-print(dfq.columns)     # Liste des colonnes
-print(dfq.head(5))     #  Aperçu des 5 premières lignes
